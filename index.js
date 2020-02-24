34210997f79145dc4ae6f3a0c1a2fd6b01c5f213
Bibek Rasaili <Bibek.Rasaili7@gmail.com>
2020-02-24 21:07:14 +0000

    navbar toggle icon - almost complete

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 document.getElementById('navbar-icon').addEventListener("click", myFunction);
 
 function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
+  var navItems = document.getElementById("myLinks");
+
+//this toggles the nav items
+//If navbar is open (block) then hide (none)
+  if (navItems.style.display === "block")
+  {
+    navItems.style.display = "none";
+  }
+  else
+  {
+    navItems.style.display = "block";
+    //else if its hidden, then show (display:block)
   }
 }
